bbb8244868fefc026a1571bb36b39f3a53e9cfc4
XU <qqbuby@gmail.com>
2022-06-06 16:00:14 +0800

    Get started with Dapr

---
+++ DaprMultiContainer/src/MyFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFrontEnd.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly DaprClient _daprClient;
+
+    public IndexModel(DaprClient daprClient)
+    {
+        _daprClient = daprClient;
+    }
+
+    public async Task OnGet()
+    {
+        var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                        HttpMethod.Get,
+                        "MyBackEnd",
+                        "weatherforecast");
+        ViewData["WeatherForecastData"] = forecasts;
+    }
+}
